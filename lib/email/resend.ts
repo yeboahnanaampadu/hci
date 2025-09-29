fabe17d4cb53a86867d14a8542efeed9fbaf621a
Ampadu Nana <ampaduyeboah60@gmail.com>
2025-09-29 21:46:41 +0000

    Remove unused getOwnerEmail function from resend.ts

--- lib/email/resend.ts
+++ lib/email/resend.ts
@@ -12,9 +12,4 @@ export function getDefaultFromEmail() {
   return email
 }
 
-export function getOwnerEmail() {
-  // For testing mode, send all emails to the owner's verified email
-  return process.env.OWNER_EMAIL || 'yeboahnanaampadu@gmail.com'
-}
-
 
